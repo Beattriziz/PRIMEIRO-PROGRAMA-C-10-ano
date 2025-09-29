8c5cbafe4d63a52c6b082f15fffa04d2384c628a
Beattriziz <beatrizcsousa2010@gmail.com>
2025-09-29 10:31:06 +0100

    Add files via upload

---
+++ primeiroprograma.cpp
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"És tão burro que a tua mãe até hoje arrepende-se de não ter usado preservativo";
+    
+    std::cout<<"Querida, és tão bela que nem o teu creme hidratante te penetra";
+
+    return 0;
+}
