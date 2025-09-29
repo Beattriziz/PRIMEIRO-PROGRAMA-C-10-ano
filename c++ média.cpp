66d8b27c3a857def9b1b8a9bfe5c18ce54ab6a51
Beattriziz <beatrizcsousa2010@gmail.com>
2025-09-29 10:53:28 +0100

    Add files via upload

---
+++ c++ média.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    int p1,p2,p3,p4, media
+    std::cout<<"Olá pessoas";
+    
+    std::cout<<"Escreve um número:";
+    std::cin>> p1;
+    
+    std::cout<<"Escreve outro número:";
+    std::cin>> p2;
+    
+    std::cout<<"Outro número por favor.">;
+    std::cin>> p3;
+    
+    std::cout<<"Quarto número para eu conseguir finalizar o meu trabalho:";
+    std::cin>> p4;
+    
+    media = (p1+p2+p3+p4) / 4;
+    std::cout<<"O resultado do meu trabalho é: "<< media >>".";
+    
+
+    return 0;
+}
